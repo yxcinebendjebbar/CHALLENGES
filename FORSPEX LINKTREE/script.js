5e57113a4de6a6ceaf6da2b62a790d438a1ae4e1
yacine bendjebbar <106481756+yacinebendjebbar@users.noreply.github.com>
2022-10-18 20:18:57 +0100

    Add files via upload

---
+++ FORSPEX LINKTREE/script.js
@@ -0,0 +1,18 @@
+let facebook = document.getElementById("fb");
+let instagram = document.getElementById("inst");
+let linkedin = document.getElementById("lin");
+let website = document.getElementById("wb");
+
+
+facebook.onclick = ()=>{
+    window.open('https://www.facebook.com/forspex.official','_blank')
+}
+instagram.onclick = ()=>{
+    window.open('https://www.instagram.com/forspexentertainment/','_blank')
+}
+linkedin.onclick = ()=>{
+    window.open('https://www.linkedin.com/company/forspex/','_blank')
+}
+website.onclick = ()=>{
+    window.open('https://forspex.com','_blank')
+}
